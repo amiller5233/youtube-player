e55c0d1cd02ae9f4a9bdccb460c140d899fd1c2c
Adam Miller <babaganoosh76@gmail.com>
2017-04-15 13:43:33 -0700

    Can play multiple songs w/o refreshing

--- youtube.js
+++ youtube.js
@@ -4,7 +4,7 @@ tag.src = "http://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
+var player, curId;
 // function onYouTubeIframeAPIReady(id) {
 //   player = new YT.Player('player', {
 //     height: '100',
@@ -21,6 +21,7 @@ function createPlayer(id) {
   player = new YT.Player('player', {
     height: '100',
     width: '100',
+    // videoId: 'UqyT8IEBkvY',
     videoId: id,
     events: {
       'onReady': onPlayerReady,
@@ -32,7 +33,9 @@ function createPlayer(id) {
 function onPlayerReady(event) {
   // event.target.playVideo();
   playYT();
-  $('#playPause').show();
+  // console.log('onPlayerReady');
+  curId = player.getVideoUrl().split("=").pop();
+  // console.log(curId);
 }
 
 var done = false;
@@ -41,16 +44,44 @@ function onPlayerStateChange(event) {
   //   setTimeout(stopYT, 6000);
   //   done = true;
   // }
-  console.log('onPlayerStateChange');
+  // console.log('onPlayerStateChange');
+  printPlayerState();
+}
+
+function printPlayerState() {
+  var state = player.getPlayerState();
+  switch(state) {
+    case -1:
+      console.log('unstarted');
+      break;
+    case 0:
+      console.log('ended');
+      break;
+    case 1:
+      console.log('playing');
+      $('#playPause').show();
+      break;
+    case 2:
+      console.log('paused');
+      break;
+    case 3:
+      console.log('buffering');
+      break;
+    case 5:
+      console.log('video cued');
+      break;
+    default:
+      console.log('default');
+  }
 }
 
 function playYT() {
   player.playVideo();
-  $('#playPause').find('img').attr('src', 'pics/play_p.svg');
+  $('#playPause').find('img').attr('src', 'pics/pause_p.svg');
 }
 function pauseYT() {
   player.pauseVideo();
-  $('#playPause').find('img').attr('src', 'pics/pause_p.svg');
+  $('#playPause').find('img').attr('src', 'pics/play_p.svg');
 }
 function stopYT() {
   player.stopVideo();
@@ -58,8 +89,7 @@ function stopYT() {
 
 function toggleYT() {
   var state = player.getPlayerState();
-  console.log(state);
-  switch(state){
+  switch(state) {
     case 1: 
       pauseYT();
       break;
